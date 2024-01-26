88539cb976ea0f6a885e7ec61613d58fb340c811
Arda Berktin <arberktin@gmail.com>
2024-01-25 17:02:02 -0800

    page fix try

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/experiences" element={<Experiences />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact-me" element={<ContactMe />} />
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import "../../App.css";
 import Cards from "../Cards";
 import HeroSection from "../HeroSection";
 
 function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to the top when the route changes
+  }, [location.pathname]);
+
   return (
     <>
       <HeroSection />
