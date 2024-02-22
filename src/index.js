f6a8a3dbedee5693bf1ad50484d39f4330715002
Arda Berktin <arberktin@gmail.com>
2024-02-22 02:05:11 -0800

    uncommented GA initialization

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom";
 import App from "./App";
-// import ReactGA from "react-ga";
+import ReactGA from "react-ga";
 
-// ReactGA.initialize("G-P7Z398R6GK");
+ReactGA.initialize("G-P7Z398R6GK");
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
