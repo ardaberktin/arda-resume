1bf8720b4acc9a8665d4d171dc61ea16b7d8c267
Arda Berktin <arberktin@gmail.com>
2024-05-21 17:51:45 +0300

    fixing navbar crash with burger button on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,7 +61,7 @@ function Navbar() {
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             <img className="arda-icon" src="images/ardaface.png" alt="Arda" />
-            ARDA BERKTIN
+            <p>ARDA BERKTIN</p>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
