650b09ba537c6005f14c7daf282f0ba2203928cb
Arda Berktin <arberktin@gmail.com>
2024-05-30 14:05:02 +0300

    updated the about me paragraph

--- src/components/AboutMeInfo.js
+++ src/components/AboutMeInfo.js
@@ -18,43 +18,44 @@ function AboutMeInfo() {
       </a>
 
       <p className="about-me-paragraph">
-        Hello there! I'm Arda, a dedicated software engineering student
-        currently pursuing my Bachelor's degree at the University of Victoria. I
-        believe that software systems are not just tools but crucial in serving
-        and shaping our society.
+        Hello! I'm Arda, a dedicated software engineering student pursuing my
+        Bachelor's degree at the University of Victoria. I believe that software
+        systems are not just tools but crucial in serving and shaping our
+        society.
         <br />
         <br />
-        While my journey in software engineering has been exciting, it's the
-        projects that truly define my enthusiasm. Take, for instance, the Money
-        on Track application, built from scratch using Swift and SwiftUI. This
-        project wasn't just about lines of code; it was about creating an
-        experience that empowers users to take control of their finances.
-        Integrating Firebase API for seamless data storage and incorporating an
-        AI assistant named Budget Buddy, powered by OpenAI's API, was just the
-        beginning. <br />
+        Throughout my academic journey, I have built a strong foundation in
+        programming languages such as C, C#, Java, JavaScript, and Python. I
+        have showcased my programming skills through various projects and
+        internships, including my portfolio website, video game Robber, and
+        internship at ARD Group. My experiences span from back-end, C# .Net, and
+        Java to front-end React, Angular and JavaScript. This diverse range of
+        knowledge and experience sets me apart.
         <br />
-        But beyond the projects, what defines me are my values and skills that I
-        bring to the table. I thrive in collaborative environments, where
-        collective problem-solving is not just a task but a shared passion.
-        Whether it's volunteering at 20/20 CAN Victoria, engaging in group
-        projects, or mentoring students at PCE Turkey, I've learned the
-        invaluable art of leadership, teamwork and adaptability.
         <br />
-        <br /> Yet, what truly drives me is the constant pursuit of growth.
-        Every challenge is an opportunity to learn, every setback a chance to
-        evolve. I'm not just eager to absorb new skills but to apply them to
-        real-world problems, to innovate, and to make a meaningful impact
-        through software engineering. <br />
+        Beyond the technical skills, what defines me are my values and skills. I
+        thrive in collaborative environments, where collective problem-solving
+        is not just a task but a shared passion. Whether volunteering at 20/20
+        CAN Victoria, working on a collaborative team at ARD GROUP, or mentoring
+        students at PCE Turkey, I've learned the invaluable art of leadership,
+        teamwork, and adaptability.
+        <br />
+        <br /> What truly drives me is the constant pursuit of growth. Every
+        challenge is an opportunity to learn, and every setback is a chance to
+        evolve. I'm eager to absorb new skills and apply them to real-world
+        problems, innovate, and make a meaningful impact through software
+        engineering. <br />
         <br />
         In essence, I'm not just a coder; I'm a leader, a team player, a problem
         solver, and an advocate for positive change. I believe in the power of
-        collaboration, the value of feedback, and the endless possibilities that
-        come with innovation. And as I continue my journey, I'm excited about
-        the challenges that lie ahead, the opportunities to learn, and the
-        chance to create something truly impactful.
+        collaboration, the value of feedback, and the endless possibilities of
+        innovation. As I continue my journey, I'm excited about the challenges
+        of developing scalable software solutions, opportunities to learn new
+        programming languages or frameworks, and the chance to create something
+        genuinely impactful by leveraging emerging technologies.
         <br />
-        <br /> Let's build a future where technology isn't just about lines of
-        code but about making a difference, one line at a time.
+        <br /> I believe a future where technology isn't just about lines of
+        code but about making a real difference.
       </p>
     </div>
   );
